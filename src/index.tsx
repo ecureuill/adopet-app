7d3d74cab2aec6390717b12f45017e56033a9e07
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-05-25 14:10:11 -0300

    💄 Add initial css variables and normalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import './styles/settings/colors.css';
+import './styles/settings/fonts.css';
+import './styles/settings/sizes.css';
+import './styles/settings/spacings.css';
+import './styles/generics/normalize.css';
 
 if (process.env.NODE_ENV !== 'production') {
 	const axe = require('@axe-core/react');
