b241a8b7b5a76910832d76d7cb97d946acde72ae
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-05-25 14:04:54 -0300

    🎉 Initialize adopet project

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
+if (process.env.NODE_ENV !== 'production') {
+	const axe = require('@axe-core/react');
+	axe(React, ReactDOM, 1000);
+}
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
+
+if (process.env.NODE_ENV !== 'production') {
+	const axe = require('@axe-core/react');
+	axe(React, ReactDOM, 1000);
+}
+
 root.render(
-  <React.StrictMode>
-    <h1>Adopet</h1>
-  </React.StrictMode>
+	<React.StrictMode>
+		<p>Adopet</p>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
