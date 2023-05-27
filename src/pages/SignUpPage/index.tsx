c970b283095032694bbb199ec6325d9285f3f18c
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-05-26 22:13:17 -0300

    💄 Create SignUP page

---
+++ src/pages/SignUpPage/index.tsx
@@ -0,0 +1,23 @@
+import { Button, Logo, TextInput } from '../../components';
+import data from '../../i18n/pt-br.json';
+
+const SignUpPage = (): JSX.Element => {
+	return (
+		<main>
+			<Logo />
+			<span>
+				<p>{data.signup_msg_question}</p>
+				<p>{data.signup_msg_answer}</p>
+			</span>
+			<form className='form -flex-column -gap-big'>
+				<TextInput label={data.email} type='email'/>
+				<TextInput label={data.name}/>
+				<TextInput label={data.password} type='password'/>
+				<TextInput label={data.password_confirm} type='password'/>
+				<Button type='submit'>{data.signup}</Button>
+			</form>
+		</main>
+	);
+};
+
+export default SignUpPage;
