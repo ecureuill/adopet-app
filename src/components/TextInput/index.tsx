724bffffd51b946b38d175746e6836c832b8c590
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-05-29 23:06:01 -0300

    💄 Add reveal icon on password inputs

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,5 +1,5 @@
 import { useId } from 'react';
-
+import  { ReactComponent as EyeIco } from '../../assets/images/eye.svg';
 import './styles.css';
 
 export interface TextInputProps extends React.ComponentPropsWithoutRef<'input'> {
@@ -8,13 +8,26 @@ export interface TextInputProps extends React.ComponentPropsWithoutRef<'input'> 
 	align?: 'default' | 'center'
 	labelColor?: 'default' | 'dark',
 } 
-const TextInput = ( {label, className: styles = '', variant = 'default', align = 'default', labelColor = 'default',...rest}: TextInputProps): JSX.Element => {
+const TextInput = ( {type, label, className: styles = '', variant = 'default', align = 'default', labelColor = 'default',...rest}: TextInputProps): JSX.Element => {
 	const id = useId();
 
 	return (
 		<div className={'input-wrapper -flex-column -gap-small'}>
 			<label htmlFor={id} className={`label -label-align-${align} -label-color-${labelColor}`}>{label}</label>
-			<input id={id} {...rest} className={`input -input-${variant} -input-${align} ${styles}`} />
+			{
+				type === 'password'?
+					<div className={'input-wrapper-password'}>
+						<input id={id} {...rest} className={`input -input-${variant} -input-${align} ${styles}`} 
+							autoComplete='on'
+
+						/>
+						<div className='reveal-ico-wrapper'>
+							<EyeIco className='reveal-ico'/>
+						</div> 
+					</div> 
+					:
+					<input id={id} {...rest} className={`input -input-${variant} -input-${align} ${styles}`} />
+			}
 		</div>
 	);
 };
