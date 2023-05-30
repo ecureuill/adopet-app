3f70ba55c952114cb959db624ab4f2755195dbbe
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-05-30 16:58:41 -0300

    🔐 Update env files

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,9 @@
+
+
 export const apiSettings = {
-	baseURL: process.env.BASE_URL,
-	port: process.env.NODE_ENV === 'development'? process.env.DEV_PORT :
-		process.env.NODE_ENV === 'test'? process.env.TEST_PORT :
-			process.env.PROD_PORT
+	baseURL: process.env.REACT_APP_API_BASE_URL,
+	port: process.env.REACT_APP_API_PORT
 };
+
+console.debug(apiSettings);
 
