d4c08c0d24b480d72ecb8991214c1fc96d6893e2
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-05-30 22:06:41 -0300

    ✨ Create login logic

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,9 +1,38 @@
 import { FormEvent } from 'react';
+import { useContext } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Button, Form, Header, Logo, TextInput } from '../../components';
+import { AuthContext } from '../../context/auth.context';
 import data from '../../i18n/pt-br.json';
 import './styles.css';
 
 const LoginPage = (): JSX.Element => {
+	const navigate = useNavigate();
+	const location = useLocation();
+	const authContext = useContext(AuthContext);
+
+	const from = location.state?.from?.pathname || '/';
+
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		console.debug(from);
+		event.preventDefault();
+
+		const formData = new FormData(event.currentTarget);
+		const email = formData.get('email') as string;
+		const password = formData.get('password') as string;
+
+		if(await authContext.signIn(email, password)){
+			if(from === '/')
+				navigate('/pets', { replace: true });
+			else 
+				navigate(from, { replace: true });
+		}
+		else
+		{
+			console.debug(authContext.error);
+		}
+	};
+
 	return (
 		<>
 			<Header paws={true} />
@@ -13,15 +42,13 @@ const LoginPage = (): JSX.Element => {
 				<Form 
 					color='white'
 					submitButtonLabel={data.login}
-					submitHandler={(event: FormEvent<HTMLFormElement>) => {
-						throw new Error('Function not implemented.');
-					} } 
+					submitHandler={(event) => handleSubmit(event)} 
 					onFormInvalid={(event: FormEvent<HTMLFormElement>) => {
 						throw new Error('Function not implemented.');
 					} }>
-					<TextInput label={data.email} placeholder={data.email_login_hint} align='center' labelColor='dark' type='email'/>
-					<TextInput label={data.password} placeholder={data.password_login_hint} align='center' labelColor='dark' type='password'/>
-					<a href='#'>{data.password_forget}</a>
+					<TextInput name='email' label={data.email} placeholder={data.email_login_hint} align='center' labelColor='dark' type='email'/>
+					<TextInput name='password' label={data.password} placeholder={data.password_login_hint} align='center' labelColor='dark' type='password'/>
+					<a href='/forget'>{data.password_forget}</a>
 				</Form>
 			</main>
 		</>
