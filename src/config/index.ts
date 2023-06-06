1105646028659fb3b1d5fcfff372d43dfd45bd51
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-06 09:58:29 -0300

    🔐 add API's system user

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
-
-
 export const apiSettings = {
 	baseURL: process.env.REACT_APP_API_BASE_URL,
-	port: process.env.REACT_APP_API_PORT
+	port: process.env.REACT_APP_API_PORT,
+	user: process.env.REACT_APP_API_USER,
+	pwd: process.env.REACT_APP_API_PWD
 };
 
 console.debug(apiSettings);
