5afdb538a280020e6ad8a7ebbdda3184fb3495a7
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-06 09:29:27 -0300

    ✨ (Text component) Add subtitle variant

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -2,20 +2,25 @@ import { PropsWithChildren } from 'react';
 import './styles.css';
 
 type TextProps = {
-	variant: 'title' | 'body' | 'footer'
+	variant: 'title' | 'subtitle' | 'body' | 'footer'
 	size?: 'default' | 'small' | 'smaller'
 	id?: string
+	className?: string
 };
 
-const Text = ( {variant, size = 'default', children}: TextProps & PropsWithChildren): JSX.Element => {
-	
+const Text = ( {variant, size = 'default', children, className='', id}: TextProps & PropsWithChildren): JSX.Element => {
 	if(variant === 'title')
 		return (
-			<h1 className={`title -title-${size}`}>{children}</h1>
+			<h1 id={id} className={`title -title-${size} ${className}`}>{children}</h1>
+		);
+
+	if(variant === 'subtitle')
+		return (
+			<h2 id={id} className={`subtitle -subtitle-${size} ${className}`}>{children}</h2>
 		);
 
 	return (
-		<p className={`body -body-${size}`}>{children}</p>
+		<p id={id} className={`body -body-${size} ${className}`}>{children}</p>
 	);
 };
 
