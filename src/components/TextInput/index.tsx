447b8a090b208e0dcb3923c98173a09b49015a60
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-06 09:50:01 -0300

    ✨ (TextInput component) Add reveal button to input type=password; Add input type=tel

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useId } from 'react';
-import  { ReactComponent as EyeIco } from '../../assets/images/eye.svg';
+import { PropsWithChildren, useId } from 'react';
+import { ReactComponent as EyeIco } from '../../assets/images/eye.svg';
+import data from '../../i18n/pt-br.json';
+import { phoneRegex } from '../../utils';
 import './styles.css';
 
 export interface TextInputProps extends React.ComponentPropsWithoutRef<'input'> {
@@ -20,33 +22,76 @@ const TextInput = ( {type, errormessage, label, className: styles = '', variant 
 
 	};
 
-	return (
-		<div className={'input-wrapper -flex-column -gap-small'}>
-			<label htmlFor={id1} className={`label -label-align-${align} -label-color-${labelColor}`}>{label}</label>
-			{
-				type === 'password'?
-					<div className={'input-wrapper-password'}>
-						<input 
-							autoComplete='on'
-							{...rest} 
-							id={id1}  
-							{...properties}
-						/>
-						<div className='reveal-ico-wrapper'>
-							<EyeIco className='reveal-ico'/>
-						</div> 
-					</div> 
-					:
+	const wrapperProps = {
+		id1: id1, 
+		id2: id2, 
+		align: align, 
+		labelColor: labelColor, 
+		label: label, 
+		errormessage: errormessage,
+	};
+
+	if(type === 'password')
+		return (
+			<Wrapper {...wrapperProps}>
+				<div className={'input-wrapper-password'}>
 					<input 
+						autoComplete='on'
 						{...rest} 
-						id={id1}
+						id={id1}  
 						{...properties}
 					/>
-			}			
+					<div className='reveal-ico-wrapper'>
+						<EyeIco className='reveal-ico' role='button' aria-label={data.password_reveal}/>
+					</div> 
+				</div> 
+			</Wrapper>
+		);
+
+
+	if(type === 'tel')
+		return (
+			<Wrapper {...wrapperProps}>
+				<input 
+					autoComplete='on'
+					minLength={8}
+					maxLength={14}
+					pattern={phoneRegex}
+					{...rest} 
+					id={id1}  
+					{...properties}
+				/>
+			</Wrapper>
+		);
+	
+	return (
+		<Wrapper {...wrapperProps}>
+			<input 
+				{...rest} 
+				id={id1}
+				{...properties}
+			/>
+		</Wrapper>
+	);
+};
+
+type WrapperProps = {
+	id1: string
+	id2: string
+	align?: 'default' | 'center'
+	labelColor?: 'default' | 'dark',
+	label: string
+	errormessage?: string
+}
+const Wrapper = ({id1,id2, align, labelColor, label, errormessage, children}: WrapperProps & PropsWithChildren) => {
+	return	(
+		<div className={'input-wrapper -flex-column -gap-small'}>
+			<label htmlFor={id1} className={`label -label-align-${align} -label-color-${labelColor}`}>{label}</label>
+			{children}			
 			<div aria-live='polite'>
 				<label id={id2} className='errormessage' aria-label='email'>{errormessage}</label>
 			</div>
-
+	
 		</div>
 	);
 };
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,3 +1,5 @@
 export const bufferToURL = (data: Buffer) => {
 	return URL.createObjectURL(new Blob([new Uint8Array(data)]));
 };
+
+export const phoneRegex = '^\\s*(\\d{2}|\\d{0})[-. ]?(\\d{5}|\\d{4})[-. ]?(\\d{4})[-. ]?\\s*$';
