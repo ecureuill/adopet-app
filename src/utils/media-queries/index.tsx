5d2cb0aabb13a271c00f257d6a89ffafa8b40ef9
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-08 09:52:49 -0300

    📱 (Media Queries) Add new breakpoints

--- src/utils/media-queries/index.tsx
+++ src/utils/media-queries/index.tsx
@@ -6,6 +6,17 @@ export const Desktop = ({children}: PropsWithChildren): JSX.Element =>  {
 	return <>{isDesktop && children}</>;
 };
 
+export const DesktopOrAbove = ({children}: PropsWithChildren): JSX.Element =>  {
+	const isDesktop = useDesktopAndAboveMediaQuery();
+	return <>{isDesktop && children}</>;
+};
+
+
+export const BigDesktop = ({children}: PropsWithChildren): JSX.Element =>  {
+	const isDesktop = useDesktopMediaQuery();
+	return <>{isDesktop && children}</>;
+};
+
 export const Tablet = ({ children } : PropsWithChildren) => {
 	const isTablet = useTabletMediaQuery();
 	return <>{isTablet && children}</>;
@@ -16,15 +27,28 @@ export const Mobile = ({ children } : PropsWithChildren) => {
 	return <>{isMobile && children}</>;
 };
 
+export const SmallMobile = ({ children } : PropsWithChildren) => {
+	const isMobile = useMobileSmallMediaQuery();
+	return <>{isMobile && children}</>;
+};
+
 export const TabletOrAbove = ({ children } : PropsWithChildren) => {
 	const isTablet = useTabletAndAboveMediaQuery();
 	return <>{isTablet && children}</>;
 };
 
-export const useMobileMediaQuery = () => useMediaQuery({ maxWidth: 768 });
+export const useMobileSmallMediaQuery = () => useMediaQuery({ maxHeight: 794 });
 
-export const useTabletMediaQuery = () => useMediaQuery({ minWidth: 768, maxWidth: 1440 });
+export const useMobileMediaQuery = () => useMediaQuery({ maxWidth: 480 });
 
-export const useDesktopMediaQuery = () => useMediaQuery({ minWidth: 1440 });
+export const useTabletMediaQuery = () => useMediaQuery({ minWidth: 481, maxWidth: 768 });
 
-export const useTabletAndAboveMediaQuery = () => useMediaQuery({ minWidth: 768 });
+export const useLaptopMediaQuery = () => useMediaQuery({ minWidth: 769, maxWidth: 1024 });
+
+export const useDesktopMediaQuery = () => useMediaQuery({ minWidth: 1025, maxWidth: 1200 });
+
+export const useDesktopBigMediaQuery = () => useMediaQuery({ minWidth: 1201});
+
+export const useTabletAndAboveMediaQuery = () => !useMobileMediaQuery();
+
+export const useDesktopAndAboveMediaQuery = () => useMediaQuery({ maxWidth: 1200 });
