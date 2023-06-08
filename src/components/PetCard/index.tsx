682b0c549e32e2470116c1b8180add5d6a356907
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-08 14:25:51 -0300

    🐛 (PetCard) fix link to contact shelter

--- src/components/PetCard/index.tsx
+++ src/components/PetCard/index.tsx
@@ -35,7 +35,7 @@ const PetCard = ( {name, age, ageUnit, size, behavior, city, state, children}: P
 				<div className='card-shelter'>
 					<p className='shelter-location'>{city} ({state})</p>
 					{ authenticated &&
-						<IconLink link={'/contact'} label={data.talk_to_shelter}>
+						<IconLink link={'contact'} label={data.talk_to_shelter}>
 							<MessageIco />
 						</IconLink>
 					}
