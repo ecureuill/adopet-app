41ea0a2c21ae18b7e779d611953cbb66774de089
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-07 22:25:40 -0300

    ✨ (Text) Add containerSize parameter
    When containerSize is set to fixed, box max-width will be set to 350px

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -4,11 +4,16 @@ import './styles.css';
 type TextProps = {
 	variant: 'title' | 'subtitle' | 'body' | 'footer'
 	size?: 'default' | 'small' | 'smaller'
+	containerSize?: 'fixed' | 'unset'
 	id?: string
 	className?: string
 };
 
-const Text = ( {variant, size = 'default', children, className='', id}: TextProps & PropsWithChildren): JSX.Element => {
+const Text = ( {variant, size = 'default', containerSize = 'unset',  children, className='', id}: TextProps & PropsWithChildren): JSX.Element => {
+	
+	if(containerSize === 'fixed')
+		className = `fixed-width ${className}`;
+	
 	if(variant === 'title')
 		return (
 			<h1 id={id} className={`title -title-${size} ${className}`}>{children}</h1>
